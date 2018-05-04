b19c621bd2e987e571a0033c200e0e58c0a10a2c
MDeLaCruz92 <delacruzm10@gmail.com>
2018-05-04 13:23:32 -0400

    .DS_Store banished!

---
+++ EmojiArtwork/Views/EmojiCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArtwork
+//
+//  Created by Michael De La Cruz on 5/4/18.
+//  Copyright © 2018 Michael De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+        
+}
